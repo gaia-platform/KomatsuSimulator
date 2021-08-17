47c59fec085a329b6443f865acc69f90a93f1e02
Kenneth Yang <kenneth@gaiaplatform.io>
2021-08-16 17:48:44 -0700

    updated camera config, changed camera reference in code to public

--- KomatsuSimulator/Assets/Scripts/DumpTruckController.cs
+++ KomatsuSimulator/Assets/Scripts/DumpTruckController.cs
@@ -1,13 +1,11 @@
-using System;
 using UnityEngine;
 
 public class DumpTruckController : MonoBehaviour
 {
     // SECTION: Component References
     // Cameras
-    [SerializeField] private Camera thirdPersonCamera;
-
-    [SerializeField] private Camera topDownCamera;
+    public GameObject thirdPersonCamera;
+    public GameObject topDownCamera;
 
     // Axles
     [SerializeField] private GameObject frontRightWheel;
@@ -41,13 +39,13 @@ public class DumpTruckController : MonoBehaviour
         float turn = Input.GetAxis("Horizontal");
 
         // Handle forward/backward driving
-        truckRigidbody.AddForce(transform.forward * thrust * forwardDrive); // Add force
+        truckRigidbody.AddForce(transform.forward * (thrust * forwardDrive)); // Add force
         float overSpeed = truckRigidbody.velocity.sqrMagnitude - 100; // Check if over speed-limit of 10 m/s
         if (overSpeed > 0) // If so, apply counter force to keep at 10 m/s
         {
-            truckRigidbody.AddForce(transform.forward * overSpeed * forwardDrive * -1);
+            truckRigidbody.AddForce(transform.forward * (overSpeed * forwardDrive * -1));
         }
-        
+
         // Rotate wheels to speed
         var wheelRotateSpeed = new Vector3(truckRigidbody.velocity.sqrMagnitude * thrust * Time.fixedDeltaTime, 0, 0);
         backAxle.transform.Rotate(wheelRotateSpeed);
@@ -56,11 +54,11 @@ public class DumpTruckController : MonoBehaviour
         frontLeftWheel.transform.Rotate(wheelRotateSpeed);
 
         // Handle turning
-        truckRigidbody.AddTorque(transform.up * torque * turn); // Add torque
+        truckRigidbody.AddTorque(transform.up * (torque * turn)); // Add torque
         float overTorque = Mathf.Abs(truckRigidbody.angularVelocity.y) - 0.3f; // check if over speed-limit
         if (overTorque > 0) // If so, apply counter torque to keep at this speed
         {
-            truckRigidbody.AddTorque(transform.up * overTorque * torque * turn * -1);
+            truckRigidbody.AddTorque(transform.up * (overTorque * torque * turn * -1));
         }
     }
 }
