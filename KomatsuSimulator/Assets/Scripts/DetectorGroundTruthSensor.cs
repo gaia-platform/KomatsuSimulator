cf67c49508ccda470662447e41f944ee4838cda1
telemething <mark@telemething.com>
2021-08-22 19:00:36 -0400

    Game objects published, not yet complete (id,size)

--- KomatsuSimulator/Assets/Scripts/DetectorGroundTruthSensor.cs
+++ KomatsuSimulator/Assets/Scripts/DetectorGroundTruthSensor.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class DetectorGroundTruthSensor : MonoBehaviour
 {
+    // the name of the ros topic which to publish
+    public string topic;
+    
+    // the name of the tag which marks game objects as detectable
+    public string tagName;
+
+    Unity.Robotics.ROSTCPConnector.ROSConnection m_Ros;
+
     public RosMessageTypes.Vision.Detection3DArrayMsg FindAllObjects(string tagName)
     {
         RosMessageTypes.Vision.Detection3DArrayMsg detectedObjectArray =
@@ -12,19 +20,47 @@ public class DetectorGroundTruthSensor : MonoBehaviour
         GameObject[] gos;
         gos = GameObject.FindGameObjectsWithTag(tagName);
 
-        detectedObjectArray.detections = new RosMessageTypes.Vision.Detection3DMsg[4];
-
         GameObject closest = null;
         float distance = Mathf.Infinity;
         Vector3 position = transform.position;
         int index = 0;
 
+        // make an array of length equal to number of found game objects
+        detectedObjectArray.detections = new RosMessageTypes.Vision.Detection3DMsg[gos.GetLength(0)];
+
+        // for each found game object
         foreach (GameObject go in gos)
         {
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
+            // transform position relative to ego
+            Vector3 objPos = go.transform.position - position;
+
+            //*** TODO : get game object size
+            Vector3 objSize = new Vector3(1.0f, 1.0f, 1.0f);
+
+            float curDistance = objPos.sqrMagnitude;
 
             detectedObjectArray.detections[index] = new RosMessageTypes.Vision.Detection3DMsg();
+
+            // set bbox position
+            var bbPosition = new RosMessageTypes.Geometry.PointMsg(objPos.x, objPos.y, objPos.z);
+
+            // set orientation to identity, maybe we can improve this, not sure if it makes sense to do so
+            var bbOrientation = new RosMessageTypes.Geometry.QuaternionMsg(
+                Quaternion.identity.x, Quaternion.identity.y, Quaternion.identity.z, Quaternion.identity.w);
+
+            // set bbox pose and size
+            RosMessageTypes.Geometry.PoseMsg center = new RosMessageTypes.Geometry.PoseMsg(bbPosition,bbOrientation);
+            RosMessageTypes.Geometry.Vector3Msg size = new  RosMessageTypes.Geometry.Vector3Msg( objSize.x, objSize.y, objSize.z);
+
+            detectedObjectArray.detections[index].bbox = new RosMessageTypes.Vision.BoundingBox3DMsg(center,size);
+            detectedObjectArray.detections[index].is_tracking = false;
+            detectedObjectArray.detections[index].results = new RosMessageTypes.Vision.ObjectHypothesisWithPoseMsg[1];
+            //detectedObjectArray.detections[index].source_cloud
+            //detectedObjectArray.detections[index].tracking_id
+
+            //*** TODO : game object name is not correct here. We want an object type, like 'parson' or 'truck'. Maybe we 
+            //*** can use a tag for this.
+            detectedObjectArray.detections[index].results[0].id = go.gameObject.name;
 
             index++;
         }
@@ -33,11 +69,19 @@ public class DetectorGroundTruthSensor : MonoBehaviour
 
     void Start()
     {
-        
+        // get the ROS connection
+        m_Ros = Unity.Robotics.ROSTCPConnector.ROSConnection.instance;
+
+        // register the publisher
+        m_Ros.RegisterPublisher(topic, RosMessageTypes.Vision.Detection3DArrayMsg.k_RosMessageName);
     }
 
     void Update()
     {
-        
+        //*** TODO : maybe we should find on a different thread
+        var msg = FindAllObjects(tagName);
+
+        // publish
+        m_Ros.Send(topic, msg);
     }
 }
