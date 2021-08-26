eb4c2a0a13beee37f21001f9afdbd14cb2560985
Kenneth Yang <kenneth@gaiaplatform.io>
2021-08-26 12:04:45 -0700

    Draw line to closest point

--- KomatsuSimulator/Assets/Scripts/Sensors/RoiSpaces.cs
+++ KomatsuSimulator/Assets/Scripts/Sensors/RoiSpaces.cs
@@ -1,34 +1,77 @@
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using UnityEngine;
 
-namespace Assets.Scripts.Sensors
+namespace Sensors
 {
     public class RoiSpaces : MonoBehaviour
     {
-        // SECTION: Properties
+        // SECTION: Structs and enums
+        // private enum Roi
+        // {
+        //     Red,
+        //     Green,
+        //     Blue
+        // }
+        //
+        // private enum Direction
+        // {
+        //     FrontRight,
+        //     SideRight,
+        //     BackRight,
+        //     BackLeft,
+        //     SideLeft,
+        //     FrontLeft
+        // }
 
+        // private struct Obstacle
+        // {
+        //     public Vector3 ClosestPoint { get; set; }
+        //     public Roi Roi { get; set; }
+        //     public Direction Direction { get; set; }
+        //
+        //     public Obstacle(Vector3 closestPoint, Roi roi, Direction direction)
+        //     {
+        //         ClosestPoint = closestPoint;
+        //         Roi = roi;
+        //         Direction = direction;
+        //     }
+        // }
 
-        // Start is called before the first frame update
-        void Start()
+        // SECTION: Variables
+        private readonly Dictionary<string, List<Vector3>> _thisFrameObstaclePoints =
+            new Dictionary<string, List<Vector3>>();
+
+        private void FixedUpdate()
         {
+            foreach (string obstacle in _thisFrameObstaclePoints.Keys)
+            {
+                List<Vector3> obstacleHits = _thisFrameObstaclePoints[obstacle];
+                if (obstacleHits.Count > 1)
+                {
+                    obstacleHits = obstacleHits.OrderBy(hit => (hit - transform.position).sqrMagnitude).ToList();
+                }
+
+                Debug.DrawLine(transform.position, obstacleHits[0]);
+            }
+
+            _thisFrameObstaclePoints.Clear();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        }
-        
         [SuppressMessage("ReSharper", "UnusedMember.Local")]
         private void OnTriggerStay(Collider other)
         {
-            if (other.tag == "Obstacle")
+            if (!other.CompareTag("Obstacle")) return;
+
+            Vector3 hitClosestPoint = other.ClosestPoint(transform.position);
+
+            if (!_thisFrameObstaclePoints.ContainsKey(other.name))
             {
-                print(other.name);
-                Vector3 transformPosition = transform.position;
-                Vector3 hitClosestPoint = other.ClosestPointOnBounds(transformPosition);
-        
-                Debug.DrawLine(transformPosition, hitClosestPoint, Color.cyan);
+                _thisFrameObstaclePoints[other.name] = new List<Vector3>();
             }
+
+            _thisFrameObstaclePoints[other.name].Add(hitClosestPoint);
         }
     }
 }
