b7e7ece6dc70c42b47b1542b6cbc77ee49d1a20f
Simone Rondelli <simone@gaiaplatform.io>
2021-10-12 12:06:30 -0400

    Apply format & linting, migrate DDL to newer Gaia syntax

--- Ros/src/danger_zone/inc/idanger_zone.hpp
+++ Ros/src/danger_zone/inc/idanger_zone.hpp
@@ -3,33 +3,33 @@
 #include <string>
 
 /**
-* @brief Interface used to expose danger_zone core methods inside a Gaia ruleset rules
-*/
+ * @brief Interface used to expose danger_zone core methods inside a Gaia ruleset rules
+ */
 
-class iDangerZone
+class danger_zone_t
 {
 protected:
 // 1) We ignore -Wc++17-extensions here to quiet down the Gaia translator
 // 2) We mention GCC but this works for Clang as well
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wc++17-extensions"
-    //inline static const std::shared_ptr<ismartpallet> campus_ruleset_p_campus = nullptr;
+    // inline static const std::shared_ptr<ismartpallet> campus_ruleset_p_campus = nullptr;
 
     // The pointer to the live instance of the campus core class
     // TODO : This is not an example to good coding practice, need to make safe and modern
-    inline static iDangerZone* danger_zone_p = nullptr;
+    inline static danger_zone_t* danger_zone_p = nullptr;
 #pragma GCC diagnostic pop
 
 public:
     /**
-     * Class factory, this is the only method allowed for obtaining an instance of the 
+     * Class factory, this is the only method allowed for obtaining an instance of the
      * class withing a ruleset rule
-     * 
-     * @return iDangerZone*
-     * @throws 
+     *
+     * @return danger_zone_t*
+     * @throws
      * @exceptsafe yes
      */
-    static iDangerZone* get_callback_class()
+    static danger_zone_t* get_callback_class()
     {
         return danger_zone_p;
     }
@@ -40,7 +40,7 @@ public:
 
     /**
      * Call this from within a ruleset rule to send a ROS2 obstacleArray message
-     * 
+     *
      * @param[in] std::string : type_name : the name of the class of object detected
      * @param[in] uint : roi : the roi code
      * @param[in] uint : direction : the direction code
@@ -58,37 +58,37 @@ public:
      * @param[in] int32_t : sec : the time in seconds
      * @param[in] uint32_t : nsec : the number of nanoseconds since sec
      * @return void
-     * @throws 
+     * @throws
      * @exceptsafe yes
      */
-    virtual void cb_send_obstacleArray_message(
+    virtual void cb_send_obstacle_array_message(
         std::string type_name, uint roi, uint direction,
-        double posx, double posy, double posz, 
-        double sizex, double sizey, double sizez, 
+        double posx, double posy, double posz,
+        double sizex, double sizey, double sizez,
         double orientx, double orienty, double orientz, double orientw,
-        std::string frame_id, int32_t sec, uint32_t nsec 
-        ) = 0;
+        std::string frame_id, int32_t sec, uint32_t nsec)
+        = 0;
 
     /**
      * Constructor
-     * 
-     * @throws 
+     *
+     * @throws
      * @exceptsafe yes
      */
-    iDangerZone() = default;
+    danger_zone_t() = default;
 
     /**
      * Destructor, to get rid of annoying build warnings
-     * 
-     * @throws 
+     *
+     * @throws
      * @exceptsafe yes
      */
-    virtual ~iDangerZone() = default;
+    virtual ~danger_zone_t() = default;
 
     /**
      * Unit test, needs to be implemented
-     * 
-     * @throws 
+     *
+     * @throws
      * @exceptsafe yes
      */
     int demo_test()
--- Ros/src/danger_zone/src/danger_zone.ddl
+++ Ros/src/danger_zone/src/danger_zone.ddl
@@ -1,41 +1,33 @@
-create database if not exists danger_zone;
+database danger_zone
 
-use danger_zone;
+table detectron (
+    detid string
+)
 
-create table if not exists detectron (
-      detid string active
-);
+table detected (
+    count int32,
+    changed int32,
+    dobjects references dobject[]
+)
 
-create table if not exists detected (
-      count int32 active,
-      changed int32 active
-);
-
-create table if not exists dobject (
-      obid int32,
-      class_id string,
-      score float,
-      frame_id string,
-      range_id int32,
-      direction_id int32,
-      seconds int32,
-      nseconds int32,
-      pos_x float,
-      pos_y float,      
-      pos_z float,      
-      size_x float,
-      size_y float,
-      size_z float,
-      orient_x float,
-      orient_y float,
-      orient_z float,
-      orient_w float
-);
-
-create relationship if not exists detected_dobjects (
-    detected.dobjects -> dobject[],
-    dobject.detected -> detected
-);
-
-
-
+table dobject (
+    obid int32,
+    class_id string,
+    score float,
+    frame_id string,
+    range_id int32,
+    direction_id int32,
+    seconds int32,
+    nseconds int32,
+    pos_x float,
+    pos_y float,
+    pos_z float,
+    size_x float,
+    size_y float,
+    size_z float,
+    orient_x float,
+    orient_y float,
+    orient_z float,
+    orient_w float,
+    detected references detected
+)
