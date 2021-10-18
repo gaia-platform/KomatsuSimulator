264bdb341969def89f653e5cf0fad780641b6548
Simone Rondelli <69205877+simone-gaia@users.noreply.github.com>
2021-10-18 13:24:43 -0400

    Add snapshotter to full_launch.py (#83)

--- Ros/src/danger_zone/launch/bridge_launch.py
+++ Ros/src/danger_zone/launch/bridge_launch.py
@@ -1,26 +1,17 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch_ros.actions import Node
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
     unity_bridge_node = Node(
-            package='ros_tcp_endpoint',
-            executable='default_server_endpoint',
-            name='unity_bridge',
-            emulate_tty=True,
-            parameters=[
-                {'/ROS_IP': '127.0.0.1'},
-                {'/ROS_TCP_PORT': 10000},
-            ]
-        )
+        package='ros_tcp_endpoint',
+        executable='default_server_endpoint',
+        name='unity_bridge',
+        emulate_tty=True,
+        parameters=[
+            {'ROS_IP': '127.0.0.1'},
+            {'ROS_TCP_PORT': 10000},
+        ]
+    )
 
     return LaunchDescription([unity_bridge_node])
--- Ros/src/danger_zone/launch/full_launch.py
+++ Ros/src/danger_zone/launch/full_launch.py
@@ -1,35 +1,35 @@
-import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-from launch_ros.actions import Node
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
-import launch.actions
-import launch.substitutions
-import launch_ros.actions
-from ament_index_python.packages import get_package_share_directory
-
+# TODO: does not launch the Unity simulator yet.
 def generate_launch_description():
-
     unity_bridge_node = Node(
-            package='ros_tcp_endpoint',
-            executable='default_server_endpoint',
-            name='unity_bridge',
-            emulate_tty=True,
-            parameters=[
-                {'/ROS_IP': '127.0.0.1'},
-                {'/ROS_TCP_PORT': 10000},
-            ]
-        )
+        package='ros_tcp_endpoint',
+        executable='default_server_endpoint',
+        name='unity_bridge',
+        emulate_tty=True,
+        parameters=[
+            {'ROS_IP': '127.0.0.1'},
+            {'ROS_TCP_PORT': 10000},
+        ]
+    )
 
     danger_zone_node = Node(
-            package='danger_zone',
-            namespace='gaia',
-            executable='danger_zone',
-            name='danger_zone',
-            output='screen',
-            emulate_tty=True
-        )
+        package='danger_zone',
+        namespace='gaia',
+        executable='danger_zone',
+        name='danger_zone',
+        output='screen',
+        emulate_tty=True
+    )
 
-    return LaunchDescription([unity_bridge_node,danger_zone_node])
+    snapshotter_node = Node(
+        package='rosbag2_snapshot',
+        namespace='gaia',
+        executable='snapshotter',
+        name='snapshotter',
+        output='screen',
+        emulate_tty=True
+    )
+
+    return LaunchDescription([unity_bridge_node, danger_zone_node, snapshotter_node])
